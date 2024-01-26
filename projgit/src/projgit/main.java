8091326f9068153a79b653a91612d4644d7d0f86
dam1 <dam1@DAM15>
2024-01-26 12:14:07 +0100

    main v3

--- projgit/src/projgit/main.java
+++ projgit/src/projgit/main.java
@@ -3,7 +3,8 @@ package projgit;
 public class main {
 
 	public static void main(String[] args) {
-		System.out.println("modif 1");	
+		System.out.println("modif 1");
+		System.out.println("modif 2");
 	}
 	
 }
